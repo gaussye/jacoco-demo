7a9839be0903c35d3c99ff8393c89b6b7f853186
gaussye <gaussye@hotmail.com>
2023-07-22 11:02:56 +0800

    Update build-kaniko.sh

--- build-kaniko.sh
+++ build-kaniko.sh
@@ -3,7 +3,7 @@ search_dir=dockerfiles
 for entry in "$search_dir"/*
 do
   echo "building $entry"
-  /kaniko/executor --dockerfile `pwd`/$entry --context `pwd` --no-push &
+  /kaniko/executor --dockerfile `pwd`/$entry --context `pwd` --no-push
 done
 
 wait
