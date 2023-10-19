c86af89c202211c68c4a8268f3eac4e903866780
gaussye <gaussye@hotmail.com>
2023-10-19 08:13:23 +0800

    Update build-kaniko.sh

--- build-kaniko.sh
+++ build-kaniko.sh
@@ -4,6 +4,7 @@ for entry in "$search_dir"/*
 do
   echo "building $entry"
   /kaniko/executor --dockerfile `pwd`/$entry --context `pwd` --no-push --ignore-path=/var/mail --ignore-path=/var/spool/mail &
+  sleep 1s
 done
 
 wait
